346bccd395b72d207385ef2d8ab90e07da506d0d
Leo <mail>
2026-04-13 14:12:03 +0200

    Init

---
+++ site/src/main/java/com/boulangerie/site/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.boulangerie.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/produits")
+    public String produits() {
+        return "produits";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}

cfea283653172e586a35fd85f71a3c44ff2b21b2
Leo <mail>
2026-04-15 14:10:08 +0200

    modifs

--- src/main/java/com/boulangerie/site/controller/HomeController.java
+++ src/main/java/com/boulangerie/site/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.boulangerie.site.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+   @GetMapping("/")
     public String home() {
         return "index";
     }
